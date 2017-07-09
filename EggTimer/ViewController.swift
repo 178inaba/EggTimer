f8303dadce41913577e1620538046006d54c4ac3
178inaba <178inaba@users.noreply.github.com>
2017-07-09 16:33:24 +0900

    Add action

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -27,6 +27,12 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func startButtonClicked(_ sender: Any) {
+    }
 
+    @IBAction func stopButtonClicked(_ sender: Any) {
+    }
+
+    @IBAction func resetButtonClicked(_ sender: Any) {
+    }
 }
-
