d99b0bd95e0a28e8208bb97b1ce1587a117c085b
178inaba <178inaba@users.noreply.github.com>
2017-07-09 23:55:55 +0900

    Add EggTimer/PrefsViewController.swift

---
+++ EggTimer/PrefsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrefsViewController.swift
+//  EggTimer
+//
+//  Created by 178inaba on 2017/07/09.
+//  Copyright (c) 2017 178inaba. All rights reserved.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+}
